463ebcc391923dca175123aadb0dde9490d3262d
Dhruvjalan <dhruvjalan0202@gmail.com>
2025-05-25 06:55:51 +0530

    searchbar width increased

--- app/components/searchbar.jsx
+++ app/components/searchbar.jsx
@@ -7,7 +7,7 @@ export default function SearchBar({ searchQuery, setSearchQuery}) {
         placeholder="Search products or keywords"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
-        className="flex-1 border border-gray-300 rounded-lg px-4 py-2 text-gray-800 focus:ring-2 focus:ring-gray-500 focus:outline-none"
+        className="flex-1 border w-[32vw] border-gray-300 rounded-lg px-4 py-2 text-gray-800 focus:ring-2 focus:ring-gray-500 focus:outline-none"
         required
       />  
     </div>
